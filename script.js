5c13ec54b5c052f310d2c72d9130be8c4146c459
ZGS58 <s.a.m.93.5.8.sam@gmail.com>
2025-01-10 13:30:11 +0800

    Update END

--- script.js
+++ script.js
@@ -27,11 +27,11 @@ document.querySelector("#ok").addEventListener("click", function () {
       tip.textContent = `請輸入 ${min} ~ ${number} 內的數字`;
       max = number;
       if (currentHealth > 1) {
-        currentHealth--; // 每次扣除半颗心
-        renderHealthBar(); // 重新渲染血量条
+        currentHealth--; 
+        renderHealthBar(); 
       } else {
-        currentHealth--; // 每次扣除半颗心
-        renderHealthBar(); // 重新渲染血量条
+        currentHealth--; 
+        renderHealthBar(); 
         startButton.style.display = "flex";
         tip.textContent = "你輸了!";
       }
@@ -39,11 +39,11 @@ document.querySelector("#ok").addEventListener("click", function () {
       tip.textContent = `請輸入 ${number} ~ ${max} 內的數字`;
       min = number;
       if (currentHealth > 1) {
-        currentHealth--; // 每次扣除半颗心
-        renderHealthBar(); // 重新渲染血量条
+        currentHealth--; 
+        renderHealthBar(); 
       } else {
-        currentHealth--; // 每次扣除半颗心
-        renderHealthBar(); // 重新渲染血量条
+        currentHealth--; 
+        renderHealthBar(); 
         startButton.style.display = "flex";
         tip.textContent = "你輸了!";
       }
@@ -51,53 +51,45 @@ document.querySelector("#ok").addEventListener("click", function () {
   }
 });
 
-//// 像素风格心形的 SVG 模板
 
 const heartFull = `<img class = "heart" src = "./images/heart.png">`;
 const heartHalf = `<img class = "heart" src = "./images/heart1.png">`;
 const heartEmpty = `<img class = "heart" src = "./images/heart2.png">`;
 
-// 初始化血量
-const maxHealth = 5; // 满血量为 5 颗心
-let currentHealth = maxHealth * 2; // 当前血量，*2 是因为半颗心也算单位
 
-// 渲染血量条
+const maxHealth = 5; 
+let currentHealth = maxHealth * 2; 
+
 function renderHealthBar() {
   const healthBar = document.getElementById("health-bar");
-  healthBar.innerHTML = ""; // 清空当前血量条
+  healthBar.innerHTML = ""; 
   for (let i = 0; i < maxHealth; i++) {
     if (currentHealth >= (i + 1) * 2) {
-      healthBar.innerHTML += heartFull; // 满心
+      healthBar.innerHTML += heartFull; 
     } else if (currentHealth === i * 2 + 1) {
-      healthBar.innerHTML += heartHalf; // 半心
+      healthBar.innerHTML += heartHalf;
     } else {
-      healthBar.innerHTML += heartEmpty; // 空心
+      healthBar.innerHTML += heartEmpty; 
     }
   }
 }
 
 renderHealthBar();
 
-//再玩一次
 document.querySelector("#start").addEventListener("click", function () {
   console.log("重置遊戲");
 
-  // 重置答案
   answer = Math.floor(Math.random() * 100) + 1;
   console.log("新答案:", answer);
-
-  // 重置範圍
+  
   min = 1;
   max = 100;
 
-  // 重置血量
   currentHealth = maxHealth * 2;
   renderHealthBar();
 
-  // 重置提示文字
   tip.textContent = "請輸入 1 ~ 100 內的數字";
 
-  // 清空輸入框
   document.querySelector("#number").value = "";
   document.querySelector("#start").style.display = "none";
 });
